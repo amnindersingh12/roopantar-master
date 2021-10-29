1889dce3a455619bb86186666049ccb7ec8410f4
amninder <amninder@debian-BULLSEYE-live-builder-AMD64>
2021-10-29 21:02:54 +0530

    refactoring code

--- js/app.js
+++
@@ -1,9 +0,0 @@
-//js
-
-//LOADING SCREEN///////////////////
-$(window).on("load", function () {
-    $("#loading").fadeOut(1000, function () {
-        $("body").css("overflow", "auto");
-        clearForm();
-    })
-})
